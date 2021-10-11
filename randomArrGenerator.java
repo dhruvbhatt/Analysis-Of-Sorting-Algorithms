4a499ad5cd66cc493b814976f13587853fff9ba1
dhruvbhatt <63405689+dhruvbhatt@users.noreply.github.com>
2021-10-11 17:14:53 +0530

    Create randomArrGenerator.java

---
+++ randomArrGenerator.java
@@ -0,0 +1,23 @@
+package algo.Sorts;
+
+import java.util.Arrays;
+
+public class randomArrGenerator {
+    private final int max,min,size;
+    public randomArrGenerator(int max,int min,int size) {
+        //accepts range and size
+        this.max=max;
+        this.min=min;
+        this.size=size;
+    }
+
+    /**Randomly generates an array for Testing purposes*/
+    public int[]generateArray() {
+        int[] arr = new int[size];
+        for (int i = 0; i < 200; i++) {
+            //Math.random generates a number between 0=< and <1. Add min to ensure min is lowest possible value
+            arr[i] = (int) (Math.random() * (max - min + 1) + min);
+        }
+        return arr;
+    }
+}
